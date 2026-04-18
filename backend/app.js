9b33e20ade0e86bd10ba12604f8f10038e07ad2c
ahmedfetooh1 <ahmedfetooh54@gmail.com>
2026-04-18 11:41:54 +0200

    use morgan middleware for format logger and add body parser middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
 const exress = require('express');
 const app = exress();
+const bodyParser = require("body-parser")
+const morgan = require('morgan')
 
 
-app.get('/',(req,res)=>{
-    res.send('hello API !')
+// middleWare
+app.use(bodyParser.json());
+app.use(morgan('tiny'))
+
+
+require('dotenv/config')
+
+
+const api = process.env.API_URL
+
+app.post(`${api}/products`,(req,res)=>{
+    const product = {
+        id : 1, 
+        name : 'hair dresser',
+        image : 'some url'
+    }
+    res.send(product);
 })
 
 app.listen(3000,()=>{
