01aa413b86945df4e322e82b68fc5b770380580d
ahmedfetooh1 <ahmedfetooh54@gmail.com>
2026-04-24 12:51:23 +0300

    get list and one of products and post new product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,3 +1,4 @@
+const { Category } = require('../models/category.js');
 const {Product} = require('../models/product.js');
 const express = require('express');
 const router = express.Router() ;
@@ -6,7 +7,7 @@ const router = express.Router() ;
 
 
 router.get(`/`,async (req , res) => {
-    const productList = await Product.find();
+    const productList = await Product.find().select('name description -_id');
 
     if(!productList){
         res.status(500).json({success:false})
@@ -15,20 +16,40 @@ router.get(`/`,async (req , res) => {
 })
 
 
+router.get(`/:id`,async (req , res) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        res.status(500).json({success:false})
+    }
+    res.send(product)
+})
+
+
 router.post(`/`, async(req,res)=>{
-    const product = new Product({
+
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalild Category')
+
+
+    let product = new Product({
         name : req.body.name ,
+        description: req.body.description,
+        richDescription : req.body.richDescription,
         image : req.body.image ,
-        countInStock : req.body.countInStock
+        brand : req.body.brand,
+        price : req.body.price ,
+        category : req.body.category,
+        countInStock : req.body.countInStock,
+        rating : req.body.rating,
+        numReviews : req.body.numReviews,
+        isFeatured : req.body.isFeatured
     })
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err ,
-            success : false
-        })
-    })
+    product = await product.save()
+    if(!product){
+        return res.status(500).send('The Product can not be created')
+    }
+    res.send(product)
 })
 
 
